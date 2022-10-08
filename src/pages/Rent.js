af7690ad32782418ab4fcb175bb5dac1eb218e9a
gbstorey <gbstorey@asu.edu>
2022-10-08 16:16:10 -0700

    reorganize rent

--- src/pages/Rent.js
+++ src/pages/Rent.js
@@ -8,16 +8,16 @@ export default function Rent() {
     return (
     <ThemeProvider theme={theme}>
       <Box sx={{margin: "1.5rem"}}>
-      <Grid container spacing={2}>
-      <Box sx={{ marginLeft: "1.5rem", marginTop: "30px"}}>
-        <Typography variant="h2">
-          Rent
-        </Typography>
-        <Typography variant="h3"sx={{marginTop: "10px"}} >
-          Your hearth & shelter.
-        </Typography>
-      </Box>
-      </Grid>
+        <Grid container spacing={2}>
+          <Box sx={{ marginLeft: "1.5rem", marginTop: "30px"}}>
+            <Typography variant="h2">
+              Rent
+            </Typography>
+            <Typography variant="h3"sx={{marginTop: "10px"}} >
+              Your hearth & shelter.
+            </Typography>
+          </Box>
+        </Grid>
       </Box>
     </ThemeProvider >
     )

5919d90237fab1961fc35ffd273ac981be078f00
gbstorey <gbstorey@asu.edu>
2022-10-08 16:20:16 -0700

    Resize brand name and add contact page

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -16,14 +16,15 @@ export default function Navbar() {
       <AppBar position="static">
         <Toolbar>
         <Button href="/">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo"/>
         </Button>
-          <Typography variant="h4" component="div" sx={{ flexGrow: 1, marginLeft:"20px"}}>
+          <Typography variant="h2" component="div" sx={{ flexGrow: 1, marginLeft:"20px", fontWeight: "500", marginBottom:"5px"}}>
             Revitalize
           </Typography>
           <Button href="/about" color="inherit">About</Button>
           <Button href="/rent" color="inherit">Rent</Button>
           <Button href="/skills" color="inherit">Skills</Button>
+          <Button href="/contact" color="inherit">Contact Us</Button>
           <Button href="/signup" color="inherit">Sign Up</Button>
         </Toolbar>
       </AppBar>
